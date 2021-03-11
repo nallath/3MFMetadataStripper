f2b674543f5ea6530268c9ebae8acd3257972652
Jaime van Kessel <nallath@gmail.com>
2021-03-11 22:36:16 +0100

    Fix storing of namespaces in modelfile
    The ETree lib didn't retain multiple namespaces.

--- stripMetadata.py
+++ stripMetadata.py
@@ -1,7 +1,7 @@
 import argparse
 import zipfile
-import xml.etree.ElementTree as ET
 import io
+from lxml import etree
 
 namespace = "{http://schemas.microsoft.com/3dmanufacturing/core/2015/02}"
 
@@ -19,29 +19,23 @@ if __name__ == "__main__":
 
     try:
         with zipfile.ZipFile(args.source[0], "r") as source_archive, zipfile.ZipFile(args.dest[0], "w", compression = zipfile.ZIP_DEFLATED) as target_archive:
-
             model_data = source_archive.open("3D/3dmodel.model").read()
-            from io import StringIO
-            found_namespaces = dict([node for _, node in ET.iterparse(StringIO(model_data.decode()), events=['start-ns'])])
-            for found_namespace in found_namespaces:
-                ET.register_namespace(found_namespace, found_namespaces[found_namespace])
+            root = etree.fromstring(model_data)
+            
 
-
-            tree = ET.fromstring(model_data)    
             search = './/{0}metadata'.format(namespace)
-            for metadata_item in tree.findall(search):
+
+            for e in root.findall(search):
+                parent = e.getparent()
                 try:
-                    tree.remove(metadata_item)
+                    root.remove(e)
                 except:
                     pass
 
-            stream = io.BytesIO()
-            
-            new_tree = ET.ElementTree(tree)
-            new_tree.write(stream, encoding = "utf-8", xml_declaration = True)
-            new_model_data = stream.getvalue().decode("utf-8")
-
-
+            new_model_data = etree.tostring(root,
+                            pretty_print=True,
+                            xml_declaration=True,
+                            encoding='UTF-8')
 
             for source_archive_info in source_archive.infolist():
                 with source_archive.open(source_archive_info) as source_archive_file:
